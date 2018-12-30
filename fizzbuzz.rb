fead08829a103d38f668f0c31f11efedbe59b5dc
Joe Mullen <mullenjoe13@gmail.com>
2018-12-30 15:36:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,13 @@ num = 15
 def fizzbuzz(num)
   if num == 3
     print "Fizz"
+  end
   elsif num == 5
     print "Buzz"
+  end
   elsif num ==15
    print "FizzBuzz"
+ end
  else
    print "nil"
 end
